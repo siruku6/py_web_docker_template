1636d676b50311b16fde8081cd0095cb65e8c38f
siruku6 <sirukufarios@gmail.com>
2023-08-20 20:51:59 +0900

    feature #5: add the feature to switch the function on UI

--- src/st_app.py
+++ src/st_app.py
@@ -2,16 +2,14 @@
 # My first app
 Here's our first attempt at using data to create a table:
 """
-import streamlit as st
+from typing import List
 import numpy as np
+import streamlit as st
 from PIL import Image
 import cv2
 
 
-uploaded_file = st.file_uploader("Choose a image file (only `.jpg`)")
-
-
-if uploaded_file is not None:
+def image_uploading_func():
     image: Image = Image.open(uploaded_file)
 
     # Display original image
@@ -33,3 +31,41 @@ if uploaded_file is not None:
     )
     st.write("Converted Image")
     st.image(image, caption="Output", use_column_width=True)
+
+
+# Using object notation
+with st.sidebar:
+    sidebar_selectbox = st.selectbox(
+        "What would you like to do?",
+        ("Touch select box", "Upload image", "Mobile phone")
+    )
+    st.write("You selected: ", sidebar_selectbox)
+
+
+if sidebar_selectbox == "Upload image":
+    uploaded_file = st.file_uploader("Choose a image file (only `.jpg`)")
+    if uploaded_file is not None:
+        image_uploading_func()
+
+
+if sidebar_selectbox == "Touch select box":
+    option = st.selectbox(
+        "select box:",
+        [1, 2, 3]
+    )
+    st.write("You selected: ", f"{option}: {sidebar_selectbox}")
+
+    names: List[str] = ["Lita", "Kile", "Judy", "Phoenix", "Popo"]
+    checkboxes = {
+        name: None
+        for name in names
+    }
+    for name in names:
+        checkboxes[name] = st.checkbox(name)
+
+    msg: str = "Hello, "
+    for name in names:
+        if checkboxes[name]:
+            msg += name + ", "
+
+    st.write(msg)
